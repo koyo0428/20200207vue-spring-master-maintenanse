ce8fbdfcd92b65cb5f781f9f7f4c14963176ba51
koyo0428 <55940191+koyo0428@users.noreply.github.com>
2020-08-02 10:42:39 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui' // 追記
+import locale from 'element-ui/lib/locale/lang/ja' // 追記
+import 'element-ui/lib/theme-chalk/index.css' // 追記
+
+import firebase from 'firebase' //追記
+
 Vue.config.productionTip = false
+Vue.use(ElementUI, {locale}) // 追記
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCQkv5fs7aQx2lt6ZzFKjH4RTswFmNi0Mc",
+  authDomain: "gemini-de0b0.firebaseapp.com",
+  databaseURL: "https://gemini-de0b0.firebaseio.com",
+  projectId: "gemini-de0b0",
+  storageBucket: "gemini-de0b0.appspot.com",
+  messagingSenderId: "284859959372",
+  appId: "1:284859959372:web:2e24450d289cf2433bcce9"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,8 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { requiresAuth: true }
   },
   {
     path: '/about',
@@ -17,6 +19,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/currency',
+    name: 'currency',
+    component: () => import(/* webpackChunkName: "currency" */ '../views/Currency.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import(/* webpackChunkName: "signup" */ '../views/Signup.vue')
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: () => import(/* webpackChunkName: "signup" */  '../views/Signin.vue')
   }
 ]
 
@@ -24,6 +42,24 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if(requiresAuth) {
+    firebase.auth().onAuthStateChanged(function(user){
+      if(user){
+        next()
+      } else {
+        next({
+          path: 'signin',
+          query: { redirect: to.fullPath }
+        })
+      }
+    })
+  } else {
+    next()
+  }
 })
 
 export default router
